5c5bfbe3bc836226b3209a947cfdf6082182d745
risingstarohan <rohanchaudharyofficial@gmail.com>
2026-07-24 10:26:43 +0530

    Updated index.js

--- index.js
+++ index.js
@@ -1,5 +1,27 @@
-function sum(a,b){
-    return a+b;
+// function sum(a,b){
+//     return a+b;
+// }
+// const result = sum(20,50)
+// console.log("Sum = " + result)
+
+// 
+
+function login(msg, error) {
+    if (error) {
+        console.log("Error: " + error);
+    } else {
+        let randomNumber = Math.floor(Math.random() * 1000); 
+        console.log("Welcome");
+        console.log("Your Random Number is: " + randomNumber);
+    }
 }
-const result = sum(20,50)
-console.log("Sum = " + result)
+
+function loginVerification(username, password, clbk) {
+    if (username === "risingstarohan" && password === "Rohan") {
+        clbk("Success", null);
+    } else {
+        clbk(null, "Username or Password is incorrect");
+    }
+}
+
+loginVerification("risingstarohan", "Rohan", login);
