b841fee9b82418f0f7ff0209ebcce6e6d745f4b7
risingstarohan <rohanchaudharyofficial@gmail.com>
2026-07-31 10:17:17 +0530

    Updated index.js

--- index.js
+++ index.js
@@ -6,22 +6,63 @@
 
 // 
 
-function login(msg, error) {
-    if (error) {
-        console.log("Error: " + error);
-    } else {
-        let randomNumber = Math.floor(Math.random() * 1000); 
-        console.log("Welcome");
-        console.log("Your Random Number is: " + randomNumber);
-    }
-}
+// function login(msg, error) {
+//     if (error) {
+//         console.log("Error: " + error);
+//     } else {
+//         let randomNumber = Math.floor(Math.random() * 1000); 
+//         console.log("Welcome");
+//         console.log("Your Random Number is: " + randomNumber);
+//     }
+// }
 
-function loginVerification(username, password, clbk) {
-    if (username === "risingstarohan" && password === "Rohan") {
-        clbk("Success", null);
-    } else {
-        clbk(null, "Username or Password is incorrect");
-    }
-}
+// function loginVerification(username, password, clbk) {
+//     if (username === "risingstarohan" && password === "Rohan") {
+//         clbk("Success", null);
+//     } else {
+//         clbk(null, "Username or Password is incorrect");
+//     }
+// }
 
-loginVerification("risingstarohan", "Rohan", login);
+// loginVerification("risingstarohan", "Rohan", login);    
+
+
+// console.log("First")
+// setTimeout(()=>{console.log("Second"),1000})
+// for(i=0;i<10000;i++){
+//     console.log("Second")
+// }
+// console.log("Third")
+
+//callback hell error example
+// setTimeout(()=>{
+//     setTimeout(()=>{
+//         setTimeout(()=>{
+//             setTimeout(()=>{
+//                 setTimeout(()=>{
+//                     setTimeout(() => {        
+//                         setTimeout(() => {        
+//                             setTimeout(() => {        
+//                                 setTimeout(() => {  
+//                                 console.log("Nine")},1000)
+//                             console.log("Eight")},1000)
+//                        console.log("Seven") },1000)                          
+//                     console.log("Six")}, 1000);
+//                 console.log("Five")},1000);
+//             console.log("Four")},1000);
+//         console.log("Three")},1000);
+//     console.log("Two")},1000);
+// console.log("One")},1000);
+
+//Promises
+
+const myPromise = new Promise((resolve,reject)=>{
+    username = "risingstarohan";
+    password = "rohan1234"
+    if(username == "risingstarohan" && password == "rohan1234"){
+        resolve("success")
+    } else{
+        reject("username or password is incorrect")
+    }
+
+})
