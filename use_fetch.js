bc3d2bab6ccd91eb41322e1886d47e4cfd4b55a2
risingstarohan <rohanchaudharyofficial@gmail.com>
2026-08-14 10:25:18 +0530

    Update

--- use_fetch.js
+++ use_fetch.js
@@ -1,9 +1,14 @@
 const div = document.getElementById('container')
 const btn = document.getElementById('btn')
+const result = document.getElementById('result')
 console.log(div);
 async function display() {
     // div.innerHTML = `<h1 style = 'color: blue;background-color: lightgray;'>Welcome to DOM Manipulation</h1>`;
     const serverdata = await fetch('https://fakestoreapi.com/products')
-       console.log(serverdata);
+               const jsondata = await serverdata.json()
+       console.log(jsondata);
+       console.log(jsondata[0]);
+       result.innerHTML = `<div>${jsondata[0].title}</div>`;
 }
 btn.addEventListener('click', display);
+result.addEventListener('click', display);
